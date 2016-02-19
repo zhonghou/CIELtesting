2181f1da81312e5f6f5846c140ad489721bc116a
unknown <134708W@M615G407.sidm.student.local>
2016-02-19 14:39:09 +0800

    2/19/2016 Added json colorkey colorpicking, and colorscheme

--- js/script.js
+++ js/script.js
@@ -6,15 +6,21 @@ $(function () {
         $(".colourSchemeIntroHeader").append(dataList.questions[0].colourSchemeIntroHeader);
         $(".colourSchemeIntroText").append(dataList.questions[0].colourSchemeIntroText);
 
-        for (var i = 0; i < dataList.questions[0].swatches.length; i++) {
-            $("#swatchesWrap").append("<div id=\"" + i + "\"></div>");
-            $("#" + i).addClass('draggable');
-            $("#" + i).addClass('ui-widget-content');
-            $("#" + i).addClass('ui-draggable');
-            $("#" + i).addClass('ui-draggable-handle');
-            $("#" + i).css("background-color", dataList.questions[0].swatches[i]);
+        for (var u = 0; u < dataList.questions[0].answers.length; u++) {
+            $("#droppableWrap").append("<div id=\"answer" + u + "\" class=\"droppable ui-widget-header ui-droppable\" ></div>");
+            //$("#a" + u).addClass('droppable ui-widget-header');
+            $("#answer" + u).css("background-color", "#FFFF00");
         }
-    });
+
+        //for (var i = 0; i < dataList.questions[0].swatches.length; i++) {
+        //    $("#swatchesWrap").append("<div id=\"swatches" + i + "\"></div>");
+        //    $("#swatches" + i).addClass('draggable');
+        //    $("#swatches" + i).addClass('ui-widget-content');
+        //    $("#swatches" + i).addClass('ui-draggable');
+        //    $("#swatches" + i).addClass('ui-draggable-handle');
+        //    $("#swatches" + i).css("background-color", dataList.questions[0].swatches[i]);
+        //}
+    })
 
     $(".draggable").draggable({
         opacity: 0.7, helper: "clone",
@@ -24,10 +30,12 @@ $(function () {
     });
     //$("#set div").draggable({ stack: "#set div" });
     $(".droppable").droppable({
+        addclasses:"true",
         drop: function (event, ui) {
+            console.log(ui.draggable.attr("id"));
             $(this)
-               .css("background-color", ui.draggable.attr("id"))
-              //.addClass("ui-state-highlight")
+              .css("background-color", ui.draggable.attr("id"))
+              .addClass("ui-state-highlight")
               .find("p")
               .html("Dropped!");
         }
