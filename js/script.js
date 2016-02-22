4614b93470241225d5f22d2333dc5905e12b572d
zhonghou <zhong_hou@hotmail.com>
2016-02-22 16:36:53 +0800

    22/02/2016 - random question - update css

--- js/script.js
+++ js/script.js
@@ -1,35 +1,39 @@
 $(function () {
     var dataList;
-    $.getJSON("json/colourscheme.json", function (dataList) {
-        //for now only load question 1
-        $(".colourSchemeCounter").append(dataList.questions[0].question + " / " + dataList.questions.length);
-        $(".colourSchemeIntroHeader").append(dataList.questions[0].colourSchemeIntroHeader);
-        $(".colourSchemeIntroText").append(dataList.questions[0].colourSchemeIntroText);
 
-        for (var u = 0; u < dataList.questions[0].answers.length; u++) {
+    //to store question number
+    var questionNum;
+
+    $.getJSON("json/colourscheme.json", function (dataList) {
+        //random question number
+        questionNum = Math.floor((Math.random() * dataList.questions.length) + 0);
+        
+        $(".colourSchemeCounter").append(dataList.questions[questionNum].question + " / " + dataList.questions.length);
+        $(".colourSchemeIntroHeader").append(dataList.questions[questionNum].colourSchemeIntroHeader);
+        $(".colourSchemeIntroText").append(dataList.questions[questionNum].colourSchemeIntroText);
+
+        for (var u = 0; u < dataList.questions[questionNum].answers.length; u++) {
             $("#droppableWrap").append("<div id=\"answer" + u + "\" class=\"droppable ui-widget-header ui-droppable\" ></div>");
             //$("#a" + u).addClass('droppable ui-widget-header');
-
 
             $("#answer" + u).droppable({
                 drop: function (event, ui) {
                     console.log(ui.draggable.css("background-color"));
                     $(this)
-                      .css("background-color", ui.draggable.css("background-color"))
-                      .find("p")
-                      .html("Dropped!");
+                      .css("background-color", ui.draggable.css("background-color"));
+                      //.find("p")
+                      //.html("Dropped!");
                 }
             });
         }
 
-
-        for (var i = 0; i < dataList.questions[0].swatches.length; i++) {
+        for (var i = 0; i < dataList.questions[questionNum].swatches.length; i++) {
             $("#swatchesWrap").append("<div id=\"swatches" + i + "\"></div>");
             $("#swatches" + i).addClass('draggable');
             $("#swatches" + i).addClass('ui-widget-content');
             $("#swatches" + i).addClass('ui-draggable');
             $("#swatches" + i).addClass('ui-draggable-handle');
-            $("#swatches" + i).css("background-color", dataList.questions[0].swatches[i]);
+            $("#swatches" + i).css("background-color", dataList.questions[questionNum].swatches[i]);
 
             $("#swatches" + i).draggable({
                 opacity: 0.7, helper: "clone",
@@ -39,6 +43,12 @@ $(function () {
             });
         }
     })
+
+    //$("input[type=submit]")
+    //  .button()
+    //  .click(function (event) {
+          
+    //  });
 
     //$(".draggable").draggable({
     //    opacity: 0.7, helper: "clone",
